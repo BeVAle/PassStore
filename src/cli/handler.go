c54a3cb4918a6c0bc106161308e8db48240909ab
BeVAle <vlad.rotkopf@gmail.com>
2020-09-02 00:26:57 +0300

    add gitignore and add setValue

--- src/cli/handler.go
+++ src/cli/handler.go
@@ -12,6 +12,15 @@ func NewArgsHandler(arguments []string) *ArgsHandler {
 	return &ArgsHandler{arguments: arguments}
 }
 
+func (arg *ArgsHandler) DoHandler() {
+	if len(arg.arguments) == 1 {
+		fmt.Println("Welcome! Type some key for work (＾▽＾). (-h for Help ヽ(・∀・)ﾉ )")
+		return
+	}
+	arg.SetKeyFromCli()
+	arg.SetValueFromCli()
+}
+
 func (arg *ArgsHandler) SetKeyFromCli() {
 	for _, value := range arg.arguments {
 		if value[0] == '-' {
@@ -29,10 +38,22 @@ func (arg *ArgsHandler) showKeys() {
 	}
 }
 
-func (arg *ArgsHandler) DoHandler() {
-	if len(arg.arguments) == 1 {
-		fmt.Println("Welcome! Type some key for work (＾▽＾). (-h for Help ヽ(・∀・)ﾉ )")
-		return
+
+func (arg *ArgsHandler) SetValueFromCli()  {
+	for i := 1; i < len(arg.arguments); i++ {
+		if i == 1 {
+			continue
+		}
+		arg.values = append(arg.values, arg.arguments[i])
 	}
-	arg.SetKeyFromCli()
+	arg.showValues()
 }
+
+func (arg *ArgsHandler) showValues(){
+	for _, v := range arg.values {
+		fmt.Println(v)
+	}
+}
+
+
+
